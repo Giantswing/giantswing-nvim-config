613f4802f4020f7bf5412eefb74995896abfbd19
Alejandro Pino Alcalde <alejandropino@mintyhost.es>
2024-06-01 10:52:28 +0200

    did some shit

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -41,37 +41,9 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 })
 
 
--- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
---   pattern = { "*" },
---   callback = function()
---     vim.cmd "checktime"
---   end,
--- })
-
--- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
---   callback = function()
---     vim.opt_local.relativenumber = false
---   end,
--- })
---
--- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
---   callback = function()
---     vim.opt_local.relativenumber = true
---   end,
--- })
-
-
--- vim.api.nvim_create_autocmd({ "CursorMoved" }, {
---  callback = function()
---     vim.cmd [[
---       set laststatus=2
---     ]]
---  end,
--- })
-
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 40 }
+    vim.highlight.on_yank { higroup = "Visual", timeout = 150 }
   end,
 })
 
